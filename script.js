7e78df26045bd4b4f582b2648f61df3a48117e99
Matthew Gooding <goodmatt190@gmail.com>
2024-04-14 19:49:10 -0400

    Refined the design with some peer review

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function close() {
     mainMenu.style.top = '-100%';
 }
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// When the user scrolls down 30px from the top of the document, resize the navbar's padding and the logo's font size
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
